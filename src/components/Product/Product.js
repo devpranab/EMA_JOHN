14090f95d5700fda332b9af43a7c9cd5a00c35e5
Pranab Sarkargit config --global user.name Pranab <dev.pranabsk@gmail.com>
2023-05-02 12:50:33 +0530

    Display more information in the Product Component

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,9 +3,20 @@ import './Product.css';
 
 const Product = (props) => {
     //console.log(props.product);
+    const { name, img, price, stock, seller } = props.product;
     return (
-        <div>
-            <h4>{props.product.name}</h4>
+        <div className="product">
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div>
+                <h4 className="product-name">{name}</h4>
+                <br/>
+                <p><small>by : {seller}</small></p>
+                <p>${price}</p>
+                <br/>
+                <p><small>Only {stock} left in stock - Order Soon</small></p>
+            </div>
         </div>
     );
 };
