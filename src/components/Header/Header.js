0b2704c3d49439afdd56e3965551b2e63f53e238
Pranab Sarkargit config --global user.name Pranab <dev.pranabsk@gmail.com>
2023-05-02 12:04:07 +0530

    Setting up logo position, navigation, nav menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,8 +4,13 @@ import logo from '../../images/logo.png';
 
 const Header = () => {
     return (
-<div className="header">
-            <img src={logo} alt="Logo"/>
+        <div className="header">
+            <img src={logo} alt="Logo" />
+            <nav>
+                <a href="/shop">Shop</a>
+                <a href="/order">Order</a>
+                <a href="/manage">Manage Inventory</a>
+            </nav>
         </div>
     );
 };
