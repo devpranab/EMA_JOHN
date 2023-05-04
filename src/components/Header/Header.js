c6845037c9c8e1e9e92f33be7504053dc8b249c7
Pranab Sarkargit config --global user.name Pranab <dev.pranabsk@gmail.com>
2023-05-04 15:29:01 +0530

    Implement signout and redirect from private route

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import './Header.css';
 import logo from '../../images/logo.png';
 import { Link } from 'react-router-dom';
+import { UserContext } from '../../App';
 
 const Header = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    
     return (
         <div className="header">
             <img src={logo} alt="Logo" />
@@ -11,6 +14,7 @@ const Header = () => {
                 <Link to="/shop">Shop</Link>
                 <Link to="/review">Order</Link>
                 <Link to="/inventory">Manage Inventory</Link>
+                <button onClick={() => setLoggedInUser({})}>Sign-out</button>
             </nav>
         </div>
     );
