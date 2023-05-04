2093c6c0270756dccabf2ce9ac09423bf011ae86
Pranab Sarkargit config --global user.name Pranab <dev.pranabsk@gmail.com>
2023-05-04 09:42:25 +0530

    login facebook add

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,11 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import { auth } from './firebase.config';
-import { signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
+import { signInWithPopup, GoogleAuthProvider, signOut, FacebookAuthProvider } from "firebase/auth";
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-    const [newUser, setNewUser] = useState(false);
+    //const [newUser, setNewUser] = useState(false);
     const [user, setUser] = useState({
         isSignedIn: false,
         name: '',
@@ -14,8 +15,10 @@ const Login = () => {
         error: '',
         success: false
     });
+    const navigate = useNavigate();
 
     const googleProvider = new GoogleAuthProvider();
+    const fbProvider = new FacebookAuthProvider();
 
     const handleSignIn = () => {
         signInWithPopup(auth, googleProvider)
@@ -30,12 +33,25 @@ const Login = () => {
                     photo: photoURL
                 }
                 setUser(signedInUser);
+                navigate("/shop");
 
             }).catch((error) => {
                 // Handle Errors here.
                 console.log(error);
                 console.log(error.message);
             });
+    }
+
+
+    const handleFbSignIn = () => {
+        console.log("sdgffdg");
+        signInWithPopup(auth, fbProvider)
+            .then(res => {
+                console.log(res);
+            })
+            .catch(err => {
+                console.log(err);
+            })
     }
 
     const handleSignOut = () => {
@@ -53,13 +69,33 @@ const Login = () => {
         });
     }
 
+    //for email & password
+    const handleChange = () => { }
+
+    const handleSubmit = () => { }
+
+    const updateUserName = () => { }
+
     return (
         <div className='login'>
             {
                 user.isSignedIn ? <button onClick={handleSignOut}>Sign-out</button> :
-                <button onClick={handleSignIn}>Sign-in</button>
+                    <button onClick={handleSignIn}>Sign-in</button>
             }
-             {user.success && <p style={{color: "blue"}}>Successfully user {newUser ? "created" : "logged in"}!</p>}
+            <br />
+            <button onClick={handleFbSignIn}>Sign-in with Facebook</button>
+            {/* create simple login form email and password */}
+            <h2>Our own Firebase Authentication</h2>
+            {/* {user.success && <p style={{color: "blue"}}>Successfully user {newUser ? "created" : "logged in"}!</p>} */}
+
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="email" placeholder="your email" required onChange={handleChange} />
+                <br />
+                <input type="password" name="password" placeholder="your passwor" required onChange={handleChange} />
+                <br />
+                <input type="submit" value="Sign-in" />
+            </form>
+            <p>Error message!</p>
         </div>
     );
 };
